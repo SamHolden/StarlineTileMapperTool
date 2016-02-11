0d8449ded08917e339cb98cf54bbfc6142cd99ac
Sam Holden <Sam.Holden@gamma.co.uk>
2016-02-11 12:41:04 +0000

    Work commit

--- core/src/main/java/TileMapper/core/TileMapper.java
+++ core/src/main/java/TileMapper/core/TileMapper.java
@@ -6,11 +6,42 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import org.w3c.dom.Document;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+import java.io.IOException;
+
+
 public class TileMapper implements ApplicationListener {
 
 	@Override
-	public void create () {
-		
+	public void create () 
+	{
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		factory.setValidating(true);
+	    factory.setIgnoringElementContentWhitespace(true);
+	    try 
+	    {
+	        DocumentBuilder builder = factory.newDocumentBuilder();
+	        File file = Gdx.files.internal("assets/dungeon_tiles.xml").file();
+	        Document doc = builder.parse(file);
+	        // Do something with the document here.
+	    } 
+	    catch (ParserConfigurationException e) 
+	    {
+	    	
+	    } 
+	    catch (SAXException e) 
+	    {
+	    	
+	    } 
+	    catch (IOException e) 
+	    { 
+	    	
+	    }
 	}
 
 	@Override
