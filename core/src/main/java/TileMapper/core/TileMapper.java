d8078923dc0d74db7edf8a32de91b9fcddd8cb71
Sam Holden <Sam.Holden@gamma.co.uk>
2016-02-11 10:08:22 +0000

    Removed LIBGDX skeleton code

--- core/src/main/java/TileMapper/core/TileMapper.java
+++ core/src/main/java/TileMapper/core/TileMapper.java
@@ -7,14 +7,10 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 
 public class TileMapper implements ApplicationListener {
-	Texture texture;
-	SpriteBatch batch;
-	float elapsed;
 
 	@Override
 	public void create () {
-		texture = new Texture(Gdx.files.internal("libgdx-logo.png"));
-		batch = new SpriteBatch();
+		
 	}
 
 	@Override
@@ -23,12 +19,7 @@ public class TileMapper implements ApplicationListener {
 
 	@Override
 	public void render () {
-		elapsed += Gdx.graphics.getDeltaTime();
-		Gdx.gl.glClearColor(0, 0, 0, 0);
-		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(texture, 100+100*(float)Math.cos(elapsed), 100+25*(float)Math.sin(elapsed));
-		batch.end();
+		
 	}
 
 	@Override
