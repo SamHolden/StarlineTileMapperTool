21649c701dddabecb6b1b9c068a96f8571676f29
Sam Holden <Sam.Holden@gamma.co.uk>
2016-02-12 16:30:49 +0000

    Working on XML Parser

--- core/src/main/java/TileMapper/core/util/XMLUtil.java
+++ core/src/main/java/TileMapper/core/util/XMLUtil.java
@@ -1,6 +1,7 @@
 package TileMapper.core.util;
 
 import java.net.URL;
+import java.util.List;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
@@ -8,14 +9,14 @@ import org.dom4j.io.SAXReader;
 
 public class XMLUtil
 {
-	public static Document loadXMLDocumentL(URL url) throws DocumentException
+	public static Document loadXMLDocument(URL url) throws DocumentException
 	{
 		SAXReader reader = new SAXReader();
         Document document = reader.read(url);
         return document;
 	}
 	
-	public static List<T> parseXML()
+	public static <T> List<T> parseXML(Document doc, Class<T> modelClass)
 	{
 		
 	}
