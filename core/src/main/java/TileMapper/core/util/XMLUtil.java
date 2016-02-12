cd5ce94d652d1eec1d96fd00bc3ccb704e8c2b2d
Sam Holden <Sam.Holden@gamma.co.uk>
2016-02-12 10:18:13 +0000

    Finally working

--- core/src/main/java/TileMapper/core/util/XMLUtil.java
+++ core/src/main/java/TileMapper/core/util/XMLUtil.java
@@ -1,7 +1,12 @@
 package TileMapper.core.util;
 
+import java.net.URL;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.io.SAXReader;
+
 public class XMLUtil
 {
-
 
 }
